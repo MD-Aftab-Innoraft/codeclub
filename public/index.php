fbd33ef7791ec0b0e70f9c39fc7657bbcbe02605
MD-Aftab-Innoraft <mohammad.ansari@innoraft.com>
2024-04-23 10:09:43 +0530

    FT2-113-codeclub: Initial commit

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Include the autoloader for the application
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
+// Define a function that returns a new instance of the Kernel class
 return function (array $context) {
+    // Create a new instance of the Kernel class with the provided environment and debug mode
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+
